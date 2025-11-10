874eb8188290cbccbdedcb16bb91af1e2ef97b2b
2210071 <ashlypereira2@gmail.com>
2025-11-10 18:23:49 -0400

    cambios de url ahora

--- src/config/database.js
+++ src/config/database.js
@@ -4,21 +4,25 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const isLocal = process.env.NODE_ENV !== 'production'; // Detecta si estás local
+let databaseUrl = process.env.DATABASE_URL;
+
+const sequelize = new Sequelize(databaseUrl, {
     dialect: 'postgres',
-    protocol: 'postgres',
     logging: false,
-    dialectOptions: {
-        ssl: {
+    dialectOptions: isLocal
+      ? {
+          ssl: {
             require: true,
             rejectUnauthorized: false,
+          }
         }
-    },
+      : {}, // En Render, no pongas ssl
 });
 
 sequelize.authenticate()
-    .then(() => console.log('Conectado a la base de datos!'))
-    .catch(err => console.error('Error al conectar:', err));
+  .then(() => console.log('Conectado a la base de datos!'))
+  .catch(err => console.error('Error al conectar:', err));
 
 const db = initModels(sequelize);
 module.exports = { sequelize, db };
